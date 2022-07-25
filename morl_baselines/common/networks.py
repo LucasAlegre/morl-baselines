1739001dd81feac045294c0840816268e42774d7
Lucas Alegre <lucasnale@gmail.com>
2022-07-25 15:23:14 -0300

    Add mlp and cnn networks

---
+++ morl_baselines/common/networks.py
@@ -0,0 +1,70 @@
+from typing import Dict, List, Tuple, Type, Union
+import numpy as np
+import torch as th
+import torch.nn.functional as F
+from torch import nn
+
+
+def mlp(
+    input_dim: int,
+    output_dim: int,
+    net_arch: List[int],
+    activation_fn: Type[nn.Module] = nn.ReLU,
+) -> nn.Sequential:
+    """
+    Create a multi layer perceptron (MLP), which is
+    a collection of fully-connected layers each followed by an activation function.
+
+    :param input_dim: Dimension of the input vector
+    :param output_dim:
+    :param net_arch: Architecture of the neural net
+        It represents the number of units per layer.
+        The length of this list is the number of layers.
+    :param activation_fn: The activation function
+        to use after each layer.
+    :return:
+    """
+    assert len(net_arch) > 0
+    modules = [nn.Linear(input_dim, net_arch[0]), activation_fn()]
+
+    for idx in range(len(net_arch) - 1):
+        modules.append(nn.Linear(net_arch[idx], net_arch[idx + 1]))
+        modules.append(activation_fn())
+
+    if output_dim > 0:
+        last_layer_dim = net_arch[-1]
+        modules.append(nn.Linear(last_layer_dim, output_dim))
+
+    return nn.Sequential(*modules)
+
+
+class NatureCNN(nn.Module):
+    """
+    CNN from DQN nature paper: Mnih, Volodymyr, et al. "Human-level control through deep reinforcement learning." Nature 518.7540 (2015): 529-533.
+    :param observation_shape:
+    :param features_dim: Number of features extracted. This corresponds to the number of unit for the last layer.
+    """
+
+    def __init__(self, observation_shape: np.ndarray, features_dim: int = 512):
+        super(NatureCNN, self).__init__()
+        self.features_dim = features_dim
+        n_input_channels = 1 if len(observation_shape) == 2 else observation_shape[0]
+        self.cnn = nn.Sequential(
+            nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Flatten()
+        )
+        # Compute shape by doing one forward pass
+        with th.no_grad():
+            n_flatten = self.cnn(th.as_tensor(np.zeros(observation_shape)[np.newaxis]).float()).shape[1]
+
+        self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
+
+    def forward(self, observations: th.Tensor) -> th.Tensor:
+        if observations.dim() == 3:
+            observations = observations.unsqueeze(0)
+        return self.linear(self.cnn(observations / 255.0))
