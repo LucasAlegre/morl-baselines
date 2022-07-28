0ef9276767a20b72ef8f1c26ede66e5c7e6a4279
Florian Felten <>
2022-07-28 17:52:49 +0200

    Remove logs from committed fiels

--- examples/wandb/run-20220728_173605-3994x0d9/files/code/examples/pgmorl_halfcheetah.py
+++
@@ -1,6 +0,0 @@
-import mo_gym
-from morl_baselines.pgmorl.pgmorl import PGMORL
-
-if __name__ == "__main__":
-    algo = PGMORL(env_id="mo-halfcheetah-v4", num_envs=4, pop_size=3, warmup_iterations=10, evolutionary_iterations=10, limit_env_steps=1000000)
-    algo.train()
