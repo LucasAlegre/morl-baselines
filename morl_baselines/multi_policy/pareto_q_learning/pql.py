1d25b26b8d43198f8a97e693f7eebdc73589a946
Willem Röpke <willem.ropke@vub.be>
2022-11-25 18:19:29 +0000

    Implement PQL with hypervolume and pareto cardinality metrics

---
+++ morl_baselines/multi_policy/pareto_q_learning/pql.py
@@ -0,0 +1,244 @@
+import numpy as np
+from pymoo.indicators.hv import HV
+
+from morl_baselines.common.morl_algorithm import MOAgent
+from morl_baselines.common.pareto import get_non_dominated
+
+
+class PQL(MOAgent):
+    """
+    Pareto Q-learning
+    """
+
+    def __init__(
+            self,
+            env,
+            ref_point: np.ndarray,
+            gamma: float = 0.8,
+            initial_epsilon: float = 1.0,
+            epsilon_decay: float = 0.99,
+            final_epsilon: float = 0.1,
+            seed: int = None,
+            project_name: str = "MORL-baselines",
+            experiment_name: str = "Pareto Q-Learning",
+            log: bool = True,
+    ):
+        super().__init__(env)
+        # Learning parameters
+        self.gamma = gamma
+        self.epsilon = initial_epsilon
+        self.initial_epsilon = initial_epsilon
+        self.epsilon_decay = epsilon_decay
+        self.final_epsilon = final_epsilon
+
+        # Algorithm setup
+        self.seed = seed
+        self.rng = np.random.default_rng(seed)
+        self.ref_point = ref_point
+        self.hypervolume = HV(ref_point=ref_point * -1)
+
+        self.num_actions = self.env.action_space.n
+        low_bound = self.env.observation_space.low
+        high_bound = self.env.observation_space.high
+        self.env_shape = (high_bound[0] - low_bound[0] + 1, high_bound[1] - low_bound[1] + 1)
+        self.num_states = np.prod(self.env_shape)
+        self.num_objectives = self.env.reward_space.shape[0]
+        self.counts = np.zeros((self.num_states, self.num_actions))
+        self.non_dominated = [[{tuple(np.zeros(self.num_objectives))} for _ in range(self.num_actions)] for _ in
+                              range(self.num_states)]
+        self.avg_reward = np.zeros((self.num_states, self.num_actions, self.num_objectives))
+
+        # Logging
+        self.project_name = project_name
+        self.experiment_name = experiment_name
+        self.log = log
+
+        if self.log:
+            self.setup_wandb(project_name=self.project_name, experiment_name=self.experiment_name)
+
+    def get_config(self) -> dict:
+        """Get the configuration dictionary.
+
+        Returns:
+            Dict: A dictionary of parameters and values.
+        """
+        return {
+            "ref_point": list(self.ref_point),
+            "gamma": self.gamma,
+            "initial_epsilon": self.initial_epsilon,
+            "epsilon_decay": self.epsilon_decay,
+            "final_epsilon": self.final_epsilon,
+            "seed": self.seed
+        }
+
+    def score_pareto_cardinality(self, state):
+        """Compute the action scores based upon the Pareto cardinality metric.
+
+        Args:
+            state (int): The current state.
+
+        Returns:
+            ndarray: A score per action.
+        """
+        q_sets = [self.get_q_set(state, action) for action in range(self.num_actions)]
+        candidates = set().union(*q_sets)
+        non_dominated = get_non_dominated(candidates)
+        scores = np.zeros(self.num_actions)
+
+        for vec in non_dominated:
+            for action, q_set in enumerate(q_sets):
+                if vec in q_set:
+                    scores[action] += 1
+
+        return scores
+
+    def score_hypervolume(self, state):
+        """Compute the action scores based upon the hypervolume metric.
+
+        Args:
+            state (int): The current state.
+
+        Returns:
+            ndarray: A score per action.
+        """
+        q_sets = [self.get_q_set(state, action) for action in range(self.num_actions)]
+        action_scores = [self.hypervolume(-1 * np.array(list(q_set))) for q_set in q_sets]
+        return action_scores
+
+    def get_q_set(self, state, action):
+        """Compute the Q-set for a given state-action pair.
+
+        Args:
+            state (int): The current state.
+            action (int): The action.
+
+        Returns:
+            A set of Q vectors.
+        """
+        nd_array = np.array(list(self.non_dominated[state][action]))
+        q_array = self.avg_reward[state, action] + self.gamma * nd_array
+        return set([tuple(vec) for vec in q_array])
+
+    def select_action(self, state, score_func):
+        """Select an action in the current state.
+
+        Args:
+            state (int): The current state.
+            score_func (callable): A function that returns a score per action.
+
+        Returns:
+            int: The selected action.
+        """
+        if self.rng.uniform(0, 1) < self.epsilon:
+            return self.rng.integers(self.num_actions)
+        else:
+            action_scores = score_func(state)
+            return self.rng.choice(np.argwhere(action_scores == np.max(action_scores)).flatten())
+
+    def calc_non_dominated(self, state):
+        """Get the non-dominated vectors in a given state.
+
+        Args:
+            state (int): The current state.
+
+        Returns:
+            Set: A set of Pareto non-dominated vectors.
+        """
+        candidates = set().union(*[self.get_q_set(state, action) for action in range(self.num_actions)])
+        non_dominated = get_non_dominated(candidates)
+        return non_dominated
+
+    def train(self, num_episodes=3000, max_timesteps=1000, log_every=100, action_eval='hypervolume'):
+        """Learn the Pareto front.
+
+        Args:
+            num_episodes (int, optional): The number of episodes to train for.
+            max_timesteps (int, optional): The maximum number of time steps in a single episode. (Default value = 1000)
+            log_every (int, optional): Log the results every number of episodes. (Default value = 100)
+            action_eval (str, optional): The action evaluation function name. (Default value = 'hypervolume')
+
+        Returns:
+            Set: The final Pareto front.
+        """
+        if action_eval == 'hypervolume':
+            score_func = self.score_hypervolume
+        elif action_eval =='pareto_cardinality':
+            score_func = self.score_pareto_cardinality
+        else:
+            raise Exception('No other method implemented yet')
+
+        for episode in range(num_episodes):
+            if episode % log_every == 0:
+                print(f'Training episode {episode + 1}')
+
+            state, _ = self.env.reset()
+            state = int(np.ravel_multi_index(state, self.env_shape))
+            terminated = False
+            truncated = False
+            timestep = 0
+
+            while not (terminated or truncated) and timestep < max_timesteps:
+                action = self.select_action(state, score_func)
+                next_state, reward, terminated, truncated, _ = self.env.step(action)
+                next_state = int(np.ravel_multi_index(next_state, self.env_shape))
+
+                self.counts[state, action] += 1
+                self.non_dominated[state][action] = self.calc_non_dominated(next_state)
+                self.avg_reward[state, action] += (reward - self.avg_reward[state, action]) / self.counts[state, action]
+                self.epsilon = max(self.final_epsilon, self.epsilon * self.epsilon_decay)
+                state = next_state
+                timestep += 1
+
+            if self.log and episode % log_every == 0:
+                pf = self.get_local_pcs(state=0)
+                value = self.hypervolume(-1 * np.array(list(pf)))
+                print(f'Hypervolume in episode {episode}: {value}')
+                self.writer.add_scalar("train/hypervolume", value, episode)
+
+        return self.get_local_pcs(state=0)
+
+    def track_policy(self, vec):
+        """Track a policy from its return vector.
+
+        Args:
+            vec (array_like):
+        """
+        target = np.array(vec)
+        state, _ = self.env.reset()
+        terminated = False
+        truncated = False
+        total_rew = np.zeros(self.num_objectives)
+
+        while not (terminated or truncated):
+            state = np.ravel_multi_index(state, self.env_shape)
+            new_target = False
+
+            for action in range(self.num_actions):
+                im_rew = self.avg_reward[state, action]
+                nd = self.non_dominated[state][action]
+                for q in nd:
+                    q = np.array(q)
+                    if np.all(self.gamma * q + im_rew == target):
+                        state, reward, terminated, truncated, _ = self.env.step(action)
+                        total_rew += reward
+                        target = q
+                        new_target = True
+                        break
+
+                if new_target:
+                    break
+
+        return total_rew
+
+    def get_local_pcs(self, state=0):
+        """Collect the local PCS in a given state.
+
+        Args:
+            state (int, optional): The state to get a local PCS for. (Default value = 0)
+
+        Returns:
+            Set: A set of Pareto optimal vectors.
+        """
+        q_sets = [self.get_q_set(state, action) for action in range(self.num_actions)]
+        candidates = set().union(*q_sets)
+        return get_non_dominated(candidates)
