bfeca8a01a54affb66f6be80888556fe21d0b9ce
Willem Röpke <34691294+wilrop@users.noreply.github.com>
2022-12-05 12:17:52 +0000

    Implement the original PQL without support for stochastic environments (#10)
    * Add pycharm config to gitignore
    
    * Init commit on original PQL implementation
    
    * Implement PQL with hypervolume and pareto cardinality metrics
    
    * Implement example with training and policy following
    
    * Fix type of input points
    
    * Use hypervolume from common and update epsilon after a full episode
    
    * Set hyperparameters as mentioned in the paper
    
    * Remove max timesteps from training as it is part of the environment
    
    * Remove old PQL implementation

--- examples/pql_dst.py
+++ examples/pql_dst.py
@@ -3,45 +3,32 @@ import numpy as np
 from mo_gym.deep_sea_treasure.deep_sea_treasure import CONCAVE_MAP
 from pymoo.indicators.hv import HV
 
-from morl_baselines.multi_policy.pareto_q_learning.pql import ParetoQ
+from morl_baselines.multi_policy.pareto_q_learning.pql import PQL
 
 if __name__ == "__main__":
     env_id = "deep-sea-treasure-v0"
     env = mo_gym.make(env_id, dst_map=CONCAVE_MAP)
     ref_point = np.array([0, -25])
-    hypervolume = HV(ref_point=-1 * ref_point)
-    perf_indic = lambda vec_set: hypervolume(-1 * np.array(list(vec_set)))  # Pymoo flips everything.
 
-    pf = {
-        (3.0, -5.0),
-        (16.0, -9.0),
-        (2.0, -3.0),
-        (1.0, -1.0),
-        (74.0, -17.0),
-        (24.0, -13.0),
-        (50.0, -14.0),
-        (5.0, -7.0),
-        (8.0, -8.0),
-        (124.0, -19.0),
-    }
-    value = perf_indic(pf)
-
-    agent = ParetoQ(
+    agent = PQL(
         env,
-        perf_indic,
-        gamma=1,
-        init_epsilon=1,
+        ref_point,
+        gamma=1.,
+        initial_epsilon=1.,
         epsilon_decay=0.997,
-        decay_every=10,
-        min_epsilon=0.05,
-        decimals=2,
-        novec=30,
+        final_epsilon=0.2,
+        seed=1,
+        project_name="MORL-baselines",
+        experiment_name="Pareto Q-Learning",
+        log=False,
     )
-    agent.train(
-        iterations=3000,
-        max_timesteps=100,
-        log=True,
-        log_every=1,
-        project_name="PQL",
-        experiment_name="PQL",
-    )
+
+    num_episodes = 10000
+    pf = agent.train(num_episodes=10000, log_every=100, action_eval='hypervolume')
+    print(pf)
+
+    # Execute a policy
+    target = np.array(pf.pop())
+    print(f'Tracking {target}')
+    reward = agent.track_policy(target)
+    print(f'Obtained {reward}')
