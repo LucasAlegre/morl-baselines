2a30328ec7706aa1434f235d0b0c4afc085e91b5
Lucas Alegre <lucasnale@gmail.com>
2023-02-12 22:31:24 +0100

    Update gpi-pd example

--- examples/gpi_pd_minecart.py
+++ examples/gpi_pd_minecart.py
@@ -2,19 +2,15 @@ import fire
 import gymnasium as gym
 import mo_gymnasium as mo_gym
 import numpy as np
-from gymnasium.wrappers.record_video import RecordVideo
-from mo_gymnasium.evaluation import policy_evaluation_mo
-from mo_gymnasium import MORecordEpisodeStatistics
-
-
 import wandb as wb
+from gymnasium.wrappers.record_video import RecordVideo
+from mo_gymnasium import MORecordEpisodeStatistics
+from mo_gymnasium.evaluation import policy_evaluation_mo
+
+from morl_baselines.common.utils import extrema_weights, random_weights
 from morl_baselines.multi_policy.envelope.envelope import Envelope
 from morl_baselines.multi_policy.gpi_pd.gpi_pd import GPIPD
 from morl_baselines.multi_policy.linear_support.linear_support import LinearSupport
-
-
-from rl.utils.utils import (extrema_weights, random_weights, seed_everything,
-                            unique_tol)
 
 
 def best_vector(values, w):
@@ -26,16 +22,13 @@ def best_vector(values, w):
 
 
 def main(algo: str, gpi_pd: bool, g: int, timesteps_per_iter: int = 10000, seed: int = 0):
-
-    seed_everything(seed)
-
     def make_env():
         env = mo_gym.make("minecart-v0")
         env = mo_gym.MORecordEpisodeStatistics(env, gamma=0.98)
         return env
 
     env = make_env()
-    eval_env = make_env() # RecordVideo(make_env(), "videos/minecart/", episode_trigger=lambda e: e % 1000 == 0)
+    eval_env = make_env()  # RecordVideo(make_env(), "videos/minecart/", episode_trigger=lambda e: e % 1000 == 0)
 
     # print(env.convex_coverage_set(frame_skip=4, discount=0.98, incremental_frame_skip=True, symmetric=True))
 
@@ -45,7 +38,7 @@ def main(algo: str, gpi_pd: bool, g: int, timesteps_per_iter: int = 10000, seed:
         else:
             return 250
 
-    if algo == 'ols' or algo == 'gpi-ls':
+    if algo == "ols" or algo == "gpi-ls":
         agent = GPIPD(
             env,
             num_nets=2,
@@ -60,17 +53,17 @@ def main(algo: str, gpi_pd: bool, g: int, timesteps_per_iter: int = 10000, seed:
             epsilon_decay_steps=50000,
             learning_starts=100,
             alpha_per=0.6,
-            min_priority=0.01,       
+            min_priority=0.01,
             per=gpi_pd,
             gper=gpi_pd,
-            envelope=False, #algo != 'ols',
+            envelope=False,  # algo != 'ols',
             use_gpi=True,
             gradient_updates=g,
             target_net_update_freq=200,
             tau=1,
             dyna=gpi_pd,
             dynamics_uncertainty_threshold=1.5,
-            dynamics_net_arch=[256, 256, 256],#[200, 200, 200, 200],
+            dynamics_net_arch=[256, 256, 256],  # [200, 200, 200, 200],
             dynamics_buffer_size=int(1e5),
             dynamics_rollout_batch_size=25000,
             dynamics_train_freq=model_training_schedule,
@@ -82,7 +75,7 @@ def main(algo: str, gpi_pd: bool, g: int, timesteps_per_iter: int = 10000, seed:
             project_name="MORL Baselines - MineCart",
             experiment_name=f"{algo} + gpid-pd={gpi_pd} g={g}",
         )
-    elif algo == 'envelope':
+    elif algo == "envelope":
         agent = Envelope(
             env,
             num_nets=2,
@@ -109,24 +102,23 @@ def main(algo: str, gpi_pd: bool, g: int, timesteps_per_iter: int = 10000, seed:
             log=True,
             project_name="MORL Baselines - MineCart",
             experiment_name=f"{algo} g={g}",
-        )       
+        )
 
-    if algo == 'ols' or algo == 'gpi-ls':
-        linear_support = LinearSupport(num_objectives=3, epsilon=0.0 if algo == 'ols' else None)
+    if algo == "ols" or algo == "gpi-ls":
+        linear_support = LinearSupport(num_objectives=3, epsilon=0.0 if algo == "ols" else None)
 
     weight_history = []
 
-    test_tasks = random_weights(dim=3, seed=42, n=100-3) + extrema_weights(3)
-    ref_point = np.array([0.0, 0.0, -11.0])
+    test_tasks = random_weights(dim=3, seed=42, n=100 - 3, dist="dirichlet") + extrema_weights(3)
     ccs = eval_env.convex_coverage_set(frame_skip=4, discount=0.98, incremental_frame_skip=True, symmetric=True)
     max_iter = 15
     for iter in range(1, max_iter + 1):
-        if algo == 'ols' or algo == 'gpi-ls':
-            if algo == 'gpi-ls':
-                agent.set_weight_support(unique_tol(linear_support.get_weight_support()))
-                w = linear_support.next_weight(algo='gpi-ls', gpi_agent=agent, env=eval_env)
+        if algo == "ols" or algo == "gpi-ls":
+            if algo == "gpi-ls":
+                agent.set_weight_support(linear_support.get_weight_support())
+                w = linear_support.next_weight(algo="gpi-ls", gpi_agent=agent, env=eval_env)
             else:
-                w = linear_support.next_weight(algo='ols')
+                w = linear_support.next_weight(algo="ols")
 
             if w is None:
                 for i in range(iter, max_iter + 1):
@@ -138,12 +130,12 @@ def main(algo: str, gpi_pd: bool, g: int, timesteps_per_iter: int = 10000, seed:
                 break
         else:
             w = None
-            
-        print('Next weight vector:', w)
+
+        print("Next weight vector:", w)
         weight_history.append(w)
-        if algo == 'gpi-ls':
+        if algo == "gpi-ls":
             M = unique_tol(linear_support.get_weight_support() + linear_support.get_corner_weights(top_k=4) + [w])
-        elif algo == 'ols':
+        elif algo == "ols":
             M = linear_support.get_weight_support() + [w]
         else:
             M = None
@@ -152,35 +144,45 @@ def main(algo: str, gpi_pd: bool, g: int, timesteps_per_iter: int = 10000, seed:
             total_timesteps=timesteps_per_iter,
             weight=w,
             weight_support=M,
-            change_w_every_episode=algo=='gpi-ls',
+            change_w_every_episode=algo == "gpi-ls",
             eval_env=eval_env,
             eval_freq=1000,
             reset_num_timesteps=False,
-            reset_learning_starts=False
+            reset_learning_starts=False,
         )
 
-        if algo != 'envelope':
-            if algo == 'ols':
+        if algo != "envelope":
+            if algo == "ols":
                 value = policy_evaluation_mo(agent, eval_env, w, rep=5)
                 linear_support.add_solution(value, w)
-            elif algo == 'gpi-ls':
+            elif algo == "gpi-ls":
                 for wcw in M:
                     n_value = policy_evaluation_mo(agent, eval_env, wcw, rep=5)
                     linear_support.add_solution(n_value, wcw, add_not_improved=False)
-        
-        if algo != 'envelope':
-            gpi_returns_test_tasks = [policy_evaluation_mo(agent, eval_env, w, rep=5, return_scalarized_value=False) for w in test_tasks]
+
+        if algo != "envelope":
+            gpi_returns_test_tasks = [
+                policy_evaluation_mo(agent, eval_env, w, rep=5, return_scalarized_value=False) for w in test_tasks
+            ]
             mean_gpi_returns_test_tasks = np.mean([np.dot(w, q) for w, q in zip(test_tasks, gpi_returns_test_tasks)], axis=0)
             wb.log({"eval/Mean Utility GPI": mean_gpi_returns_test_tasks, "iteration": iter})
-            max_gap_gpi = max([np.dot(best_vector(ccs, w),w) - np.dot(best_vector(gpi_returns_test_tasks, w),w) for w in test_tasks])
+            max_gap_gpi = max(
+                [np.dot(best_vector(ccs, w), w) - np.dot(best_vector(gpi_returns_test_tasks, w), w) for w in test_tasks]
+            )
             wb.log({"eval/Max Gap GPI": max_gap_gpi, "iteration": iter})
 
         agent.use_gpi = False
-        no_gpi_returns_test_tasks = [policy_evaluation_mo(agent, eval_env, w, rep=5, return_scalarized_value=False) for w in test_tasks]
-        mean_no_gpi_returns_test_tasks = np.mean([np.dot(psi, w) for (psi, w) in zip(no_gpi_returns_test_tasks, test_tasks)], axis=0)
-        agent.use_gpi = algo != 'envelope'
+        no_gpi_returns_test_tasks = [
+            policy_evaluation_mo(agent, eval_env, w, rep=5, return_scalarized_value=False) for w in test_tasks
+        ]
+        mean_no_gpi_returns_test_tasks = np.mean(
+            [np.dot(psi, w) for (psi, w) in zip(no_gpi_returns_test_tasks, test_tasks)], axis=0
+        )
+        agent.use_gpi = algo != "envelope"
         wb.log({"eval/Mean Utility No GPI": mean_no_gpi_returns_test_tasks, "iteration": iter})
-        max_gap_no_gpi = max([np.dot(best_vector(ccs, w),w) - np.dot(best_vector(no_gpi_returns_test_tasks, w),w) for w in test_tasks])
+        max_gap_no_gpi = max(
+            [np.dot(best_vector(ccs, w), w) - np.dot(best_vector(no_gpi_returns_test_tasks, w), w) for w in test_tasks]
+        )
         wb.log({"eval/Max Gap No GPI": max_gap_no_gpi, "iteration": iter})
 
         agent.save(filename=f"{algo}-g={g}-gpi-pd={gpi_pd}-it={iter}-seed={seed}-minecart", save_replay_buffer=False)
