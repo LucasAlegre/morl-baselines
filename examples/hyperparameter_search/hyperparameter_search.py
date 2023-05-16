1454a01147cda9bae932e070e20de6d7bd1999c4
lowlypalace <lwlypalace@gmail.com>
2023-05-16 23:39:27 +0200

    Make logging for each worker

--- examples/hyperparameter_search/hyperparameter_search.py
+++ examples/hyperparameter_search/hyperparameter_search.py
@@ -17,6 +17,7 @@ class WorkerInitData:
     sweep_id: str
     seed: int
     config: dict
+    worker_num: int
 
 @dataclass
 class WorkerDoneData:
@@ -38,6 +39,7 @@ def train(worker_data: WorkerInitData) -> WorkerDoneData:
     seed = worker_data.seed
     group = worker_data.sweep_id
     config = worker_data.config
+    worker_num = worker_data.worker_num
 
     def make_env():
         env = mo_gym.make("minecart-v0")
@@ -52,7 +54,7 @@ def train(worker_data: WorkerInitData) -> WorkerDoneData:
     agent = Envelope(env, **config, seed=seed, group=group)
 
     # Launch the agent training
-    print("Training started")
+    print(f"Worker {worker_num}: Seed {seed}. Training agent...")
     agent.train(
         total_timesteps=100000,
         total_episodes=None,
@@ -66,11 +68,10 @@ def train(worker_data: WorkerInitData) -> WorkerDoneData:
         reset_learning_starts=False,
         verbose=False
     )
-    print("Training finished")
 
     # Get the hypervolume from the wandb run
     hypervolume = wandb.run.summary["eval/hypervolume"]
-    print("Hypervolume: {}".format(hypervolume))
+    print(f"Worker {worker_num}: Seed {seed}. Hypervolume: {hypervolume}")
 
     return WorkerDoneData(hypervolume=hypervolume)
 
@@ -84,22 +85,27 @@ def main():
     with ProcessPoolExecutor(max_workers=num_seeds) as executor:
         futures = []
         for num in range(num_seeds):
-            print("Spinning up worker {}".format(num))
+            # print("Spinning up worker {}".format(num))
             seed = seeds[num]
             futures.append(executor.submit(train, WorkerInitData(
                 sweep_id=sweep_id,
                 seed=seed,
                 config=dict(sweep_run.config),
+                worker_num=num
             )))
 
         # Get results from workers
         results = [future.result() for future in futures]
 
+    # Get the hypervolume from the results
     metrics = [result.hypervolume for result in results]
-    print("Metrics: {}".format(metrics))
+
+    # Compute the average hypervolume
+    average_hypervolume = sum(metrics) / len(metrics)
+    print("Average hypervolume: {}".format(average_hypervolume))
 
     # Log the average hypervolume to the sweep run
-    sweep_run.log(dict(hypervolume=sum(metrics) / len(metrics)))
+    sweep_run.log(dict(hypervolume=average_hypervolume))
     wandb.finish()
 
 # Load the sweep config
