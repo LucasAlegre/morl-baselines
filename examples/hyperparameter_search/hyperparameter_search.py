74055ac3909088237ae5e4a9088be1def2e18226
lowlypalace <lwlypalace@gmail.com>
2023-05-16 23:09:48 +0200

    Move to concurrent.futures

--- examples/hyperparameter_search/hyperparameter_search.py
+++ examples/hyperparameter_search/hyperparameter_search.py
@@ -1,8 +1,8 @@
-import multiprocessing
 import collections
 import random
 import yaml
 import os
+from concurrent.futures import ProcessPoolExecutor
 
 import wandb
 import numpy as np
@@ -12,7 +12,6 @@ from mo_gymnasium.utils import MORecordEpisodeStatistics
 from morl_baselines.multi_policy.envelope.envelope import Envelope
 from morl_baselines.common.utils import reset_wandb_env
 
-Worker = collections.namedtuple("Worker", ("queue", "process"))
 WorkerInitData = collections.namedtuple(
     "WorkerInitData", ("sweep_id", "seed", "config")
 )
@@ -27,12 +26,10 @@ seeds = [random.randint(0, 1000000) for _ in range(num_seeds)]
 # Set the count of the sweep agent
 count = 5
 
-def train(sweep_q, worker_q):
+def train(worker_data):
     # Reset the wandb environment variables
     reset_wandb_env()
 
-    # Get the worker data
-    worker_data = worker_q.get()
     seed = worker_data.seed
     group = worker_data.sweep_id
     config = worker_data.config
@@ -52,14 +49,14 @@ def train(sweep_q, worker_q):
     # Launch the agent training
     print("Training started")
     agent.train(
-        total_timesteps=100000,
+        total_timesteps=1000,
         total_episodes=None,
         weight=None,
         eval_env=eval_env,
         ref_point=np.array([0, 0, -200.0]),
         known_pareto_front=env.unwrapped.pareto_front(gamma=0.98),
-        num_eval_weights_for_front=100,
-        eval_freq=100000,
+        num_eval_weights_for_front=5,
+        eval_freq=1000,
         reset_num_timesteps=False,
         reset_learning_starts=False,
         verbose=False
@@ -70,8 +67,7 @@ def train(sweep_q, worker_q):
     hypervolume = wandb.run.summary["eval/hypervolume"]
     print("Hypervolume: {}".format(hypervolume))
 
-    # Send the hypervolume back to the main process
-    sweep_q.put(WorkerDoneData(hypervolume=hypervolume))
+    return WorkerDoneData(hypervolume=hypervolume)
 
 def main():
     # Get the sweep id
@@ -79,36 +75,22 @@ def main():
 
     # Spin up workers before calling wandb.init()
     # Workers will be blocked on a queue waiting to start
-    sweep_q = multiprocessing.Queue()
-    workers = []
-    for num in range(num_seeds):
-        q = multiprocessing.Queue()
-        p = multiprocessing.Process(
-            target=train, kwargs=dict(sweep_q=sweep_q, worker_q=q)
-        )
-        p.start()
-        workers.append(Worker(queue=q, process=p))
 
-    metrics = []
-    for num in range(num_seeds):
-        print("Spinning up worker {}".format(num))
-        seed = seeds[num]
-        worker = workers[num]
-        worker.queue.put(
-            WorkerInitData(
+    with ProcessPoolExecutor(max_workers=num_seeds) as executor:
+        futures = []
+        for num in range(num_seeds):
+            print("Spinning up worker {}".format(num))
+            seed = seeds[num]
+            futures.append(executor.submit(train, WorkerInitData(
                 sweep_id=sweep_id,
                 seed=seed,
                 config=dict(sweep_run.config),
-            )
-        )
+            )))
 
-        # Get metric from worker
-        result = sweep_q.get()
-        # Wait for worker to finish
-        worker.process.join()
-        # Log metric to sweep_run
-        metrics.append(result.hypervolume)
+        # Get results from workers
+        results = [future.result() for future in futures]
 
+    metrics = [result.hypervolume for result in results]
     print("Metrics: {}".format(metrics))
 
     # Log the average hypervolume to the sweep run
@@ -127,4 +109,3 @@ sweep_id = wandb.sweep(sweep=sweep_config, project="MORL-Baselines")
 
 # Run the sweep agent
 wandb.agent(sweep_id, function=main, count=count)
-
