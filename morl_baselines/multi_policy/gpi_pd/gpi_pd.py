4f3bf422f1d47932686c07402057be9ff4a423b3
Lucas Alegre <lucasnale@gmail.com>
2023-06-20 17:49:45 +0200

    remove copy

--- morl_baselines/multi_policy/gpi_pd/gpi_pd.py
+++ morl_baselines/multi_policy/gpi_pd/gpi_pd.py
@@ -581,7 +581,7 @@ class GPIPD(MOPolicy, MOAgent):
 
     def eval(self, obs: np.ndarray, w: np.ndarray) -> int:
         """Select an action for the given obs and weight vector."""
-        obs = th.tensor(obs.copy()).float().to(self.device)
+        obs = th.tensor(obs).float().to(self.device)
         w = th.tensor(w).float().to(self.device)
         if self.use_gpi:
             action = self.gpi_action(obs, w, include_w=False)
@@ -722,7 +722,7 @@ class GPIPD(MOPolicy, MOAgent):
             if self.global_step < self.learning_starts:
                 action = self.env.action_space.sample()
             else:
-                action = self._act(th.tensor(obs.copy()).float().to(self.device), tensor_w)
+                action = self._act(th.tensor(obs).float().to(self.device), tensor_w)
 
             next_obs, vec_reward, terminated, truncated, info = self.env.step(action)
 
