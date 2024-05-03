bb45c588bf4fc4747ceb7caba714628e94cd5d4a
Lucas Alegre <lucasnale@gmail.com>
2024-05-03 13:04:05 -0300

    Update setup.py

--- morl_baselines/__init__.py
+++ morl_baselines/__init__.py
@@ -1,1 +1,4 @@
 """MORL-Baselines contains various MORL algorithms and utility functions."""
+
+
+__version__ = "1.0.0"
--- setup.py
+++ setup.py
@@ -1,8 +1,32 @@
+import pathlib
+
 from setuptools import setup
+
+
+CWD = pathlib.Path(__file__).absolute().parent
+
+
+def get_version():
+    """Gets the mo-gymnasium version."""
+    path = CWD / "morl_baselines" / "__init__.py"
+    content = path.read_text()
+    for line in content.splitlines():
+        if line.startswith("__version__"):
+            return line.strip().split()[-1].strip().strip('"')
+    raise RuntimeError("bad version data in __init__.py")
 
 
 setup(
     name="morl-baselines",
+    version=get_version(),
     description="Implementations of multi-objective reinforcement learning (MORL) algorithms.",
-    version="1.0.0",
+    long_description=open("README.md").read(),
 )
+
+# python setup.py sdist
+# python setup.py bdist_wheel
+# twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+# twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+# twine upload dist/*
+
+# https://towardsdatascience.com/create-your-own-python-package-and-publish-it-into-pypi-9306a29bc116
